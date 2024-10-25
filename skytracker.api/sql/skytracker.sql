e81359224041abc88bd293b7a8c199c2442be052
eneperezv <eneperezv@gmail.com>
2024-10-25 18:40:41 -0500

    Archivo de respaldo de base de datos
     Changes to be committed:
    	modified:   skytracker.api/sql/skytracker.sql

--- skytracker.api/sql/skytracker.sql
+++ skytracker.api/sql/skytracker.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 24-10-2024 a las 07:12:06
+-- Tiempo de generación: 26-10-2024 a las 01:40:10
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -44,6 +44,36 @@ CREATE TABLE `dbo_users` (
 INSERT INTO `dbo_users` (`id_user`, `name`, `password`, `role`, `username`) VALUES
 (1, 'Eliezer Navarro', '$2a$10$DRtD6u4iWycTIrKGcrYuTOITp0JZq88lClhTIgzJ3YMvzlB7LnWOG', 'USER', 'enp');
 
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `location`
+--
+
+CREATE TABLE `location` (
+  `id` bigint(20) NOT NULL,
+  `city_name` varchar(255) DEFAULT NULL,
+  `country` varchar(255) DEFAULT NULL,
+  `latitude` double DEFAULT NULL,
+  `longitude` double DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `weather_data`
+--
+
+CREATE TABLE `weather_data` (
+  `id` bigint(20) NOT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `humidity` double DEFAULT NULL,
+  `temperature` double DEFAULT NULL,
+  `timestamp` datetime(6) DEFAULT NULL,
+  `wind_speed` double DEFAULT NULL,
+  `location_id` bigint(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
 --
 -- Índices para tablas volcadas
 --
@@ -55,6 +85,19 @@ ALTER TABLE `dbo_users`
   ADD PRIMARY KEY (`id_user`);
 
 --
+-- Indices de la tabla `location`
+--
+ALTER TABLE `location`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `weather_data`
+--
+ALTER TABLE `weather_data`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FKtp17lqu78lgdu91va5j5gvdj1` (`location_id`);
+
+--
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
@@ -63,6 +106,28 @@ ALTER TABLE `dbo_users`
 --
 ALTER TABLE `dbo_users`
   MODIFY `id_user` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `location`
+--
+ALTER TABLE `location`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `weather_data`
+--
+ALTER TABLE `weather_data`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `weather_data`
+--
+ALTER TABLE `weather_data`
+  ADD CONSTRAINT `FKtp17lqu78lgdu91va5j5gvdj1` FOREIGN KEY (`location_id`) REFERENCES `location` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
